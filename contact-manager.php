bc1136adb1bfc4c341fbabba185a99a963dce7b5
Chloe Adams <chloeadams@Chloes-MacBook-Pro.local>
2017-07-27 15:31:49 -0500

    Committing changes thus far

--- contact-manager.php
+++ contact-manager.php
@@ -17,11 +17,38 @@
 
 
 do {
+  function parseContacts($filename)
+{
+    $contacts = array();
+    // todo - read file and parse contacts
+    $handle = fopen($filename, 'r');
+    $contents = trim(fread($handle, filesize($filename)));
+    $contactsArray = explode("\n", $contents);
+    // $outputArray = [];
+    foreach($contactsArray as $key => $contact) {
+        $personArray = explode("|", $contact);
+        $phoneNumber = substr($personArray[1], 0, 3);
+        $phoneNumber .= "-";
+        $phoneNumber .= substr($personArray[1], 3, 3);
+        $phoneNumber .= "-";
+        $phoneNumber .= substr($personArray[1], 6);
+        $name = $personArray[0];
+        $contacts[$key]['name'] = $name;
+        echo "$name | $phoneNumber" . PHP_EOL;
+        $contacts[$key]['number'] = $phoneNumber;
+    }
+    return $contacts;
+}
+parseContacts('contacts.txt');
   // game picks a random number between 1 and 100
   $input = [];
   // prompts user to guess the number
+  echo "\n";
+    echo "\n";
+      echo "\n";
   fwrite(STDOUT,
-  '1 View Contacts
+  '
+  1 View Contacts
   2 Add Contacts
   3 Search Contact by name
   4 Delete Contact
